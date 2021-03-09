29cf2cbff47ed674d6192fbcf6869d6ae0ea154b
adoll65140 <a651400645>
2021-03-09 02:35:26 +0000

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+  end
+  devise_for :users
+  root 'public/homes#top'
+  get '/about' => 'public/homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
